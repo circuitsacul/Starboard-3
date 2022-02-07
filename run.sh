8c08c5a9d1c845a612fc89785e3cbff51162b25e
Circuit <circuitsacul@icloud.com>
2022-02-07 12:42:56 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,6 @@
 
 while true
 do
-    $1 -m pip install --upgrade pip
-    $1 -m pip install --upgrade poetry
-    $1 -m poetry install
     $1 -m poetry run python -m starboard $2
 
     echo "Hit CTRL+C to stop..."
