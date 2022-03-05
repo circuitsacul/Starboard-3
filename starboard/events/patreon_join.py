dbeedbe64ca536482fd80692465adef0a5f0c2dc
Circuit <circuitsacul@icloud.com>
2022-03-04 19:28:25 -0500

    Trigger an update of supporter roles on main guild join

---
+++ starboard/events/patreon_join.py
@@ -0,0 +1,48 @@
+# MIT License
+#
+# Copyright (c) 2022 TrigonDev
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import annotations
+
+from typing import cast, TYPE_CHECKING
+
+import crescent
+import hikari
+
+from starboard.database import goc_user
+from starboard.config import CONFIG
+from starboard.core.premium import update_supporter_roles
+
+if TYPE_CHECKING:
+    from starboard.bot import Bot
+
+
+plugin = crescent.Plugin("patreon-join-events")
+
+
+@plugin.include
+@crescent.event
+async def on_member_join(event: hikari.MemberCreateEvent) -> None:
+    if event.guild_id is None or event.guild_id != CONFIG.main_guild:
+        return
+
+    user = await goc_user(event.user.id, event.user.is_bot)
+    await update_supporter_roles(cast("Bot", event.app), user)
