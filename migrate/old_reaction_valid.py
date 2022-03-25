f5790eb5b8dd45ae11fe0f601009a14562a63d3d
CircuitSacul <circuitsacul@icloud.com>
2022-03-25 09:43:59 -0400

    Add future annotations statement (#47)

--- migrate/old_reaction_valid.py
+++ migrate/old_reaction_valid.py
@@ -20,6 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from __future__ import annotations
+
 
 def is_user_blacklisted(
     user_roles: set[int], role_bl: set[int], role_wl: set[int]
