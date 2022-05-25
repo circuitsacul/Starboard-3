a637de676edc82e5671195824b0ffcec9d828a67
Circuit <circuitsacul@icloud.com>
2022-05-25 17:32:07 -0400

    Reset migrations

--- starboard/database/migrations/0001/migrations.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE starboards ADD COLUMN replied_to BOOLEAN;
-UPDATE starboards SET replied_to = true;
-ALTER TABLE starboards ALTER COLUMN replied_to SET NOT NULL;
--- starboard/database/migrations/0002/migrations.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE starboards ADD COLUMN autoreact_upvote BOOLEAN;
-ALTER TABLE starboards ADD COLUMN autoreact_downvote BOOLEAN;
-ALTER TABLE starboards DROP COLUMN autoreact;
-UPDATE starboards SET autoreact_upvote = true, autoreact_downvote = true;
-ALTER TABLE starboards ALTER COLUMN autoreact_upvote SET NOT NULL;
-ALTER TABLE starboards ALTER COLUMN autoreact_downvote SET NOT NULL;
