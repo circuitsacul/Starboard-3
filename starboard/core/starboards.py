c4de0b188788ede4bfe352a56f197324b2bd6373
Circuit <circuitsacul@icloud.com>
2022-07-02 23:29:34 -0400

    force overrides nsfw protection

--- starboard/core/starboards.py
+++ starboard/core/starboards.py
@@ -151,7 +151,7 @@ async def _refresh_message_for_starboard(
     force: bool,
     premium: bool,
 ) -> None:
-    if orig_msg.is_nsfw:
+    if orig_msg.is_nsfw and config.starboard.id not in orig_msg.forced_to:
         sbchannel = await bot.cache.gof_guild_channel_wnsfw(
             config.starboard.channel_id
         )
